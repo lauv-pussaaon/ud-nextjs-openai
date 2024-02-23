3f2b6630e7b0f0d4533976be14f100bac112b4b4
Lauv <pussaaon@gmail.com>
2024-02-23 10:58:52 +0700

    Move to alternative image course from Unsplash

--- hands-on/gptgenius/app/(dashboard)/tours/[id]/page.js
+++ hands-on/gptgenius/app/(dashboard)/tours/[id]/page.js
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import TourInfo from "../../../../components/TourInfo";
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
+const UNSPLASH_API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 async function SingleTourPage({ params }) {
     const tour = await getSingleTour(params.id);
@@ -11,10 +13,13 @@ async function SingleTourPage({ params }) {
         redirect("/tours");
     }
 
-    const tourImageUrl = await generateTourImage({
-        city: tour.city,
-        country: tour.country,
-    });
+    // const tourImageUrl = await generateTourImage({
+    //     city: tour.city,
+    //     country: tour.country,
+    // });
+
+    const { data } = await axios.get(`${UNSPLASH_API}${tour.city}`);
+    const tourImageUrl = data?.results[0]?.urls?.raw;
 
     return (
         <div>
